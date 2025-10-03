ade844cd77f7fdffbf0327aefbca3d36d9903f30
Elnur Celilli <92107436+elnur8453@users.noreply.github.com>
2025-10-03 14:18:20 +0400

    Add files via upload

---
+++ task9.js
@@ -0,0 +1,36 @@
+  //Tapshiriq 1
+
+ let number=Number(prompt('Iki reqemli eded daxil edin:'));
+  while(number<100){
+     console.log(number);
+    number+=7;
+}
+ console.log(number);
+
+ //Tapshiriq 2
+
+ let N=Number(prompt('ededi dail edin:'));
+let k=0;
+ while( k<N){
+     console.log('This is Cycle');
+     k++;
+ }
+
+
+//Tapshiriq 3
+
+ let s=0;
+ let i=11;
+ while(i<=99){
+     s+=i;
+     i+=2;
+ }
+ console.log(s);
+
+ //Tapshiriq 4
+
+ let i=100;
+ while(i<=999){
+     console.log(i);
+     i+=10;
+ }
