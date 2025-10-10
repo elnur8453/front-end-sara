b7ea81c2beea5f51874f10f595f89835ff863785
Elnur Celilli <92107436+elnur8453@users.noreply.github.com>
2025-10-10 13:46:20 +0400

    Add files via upload

---
+++ task11.js
@@ -0,0 +1,12 @@
+//Tapshiriq 1
+let metn = '32, 31, 34, 36, 31';
+let yeniMetn = metn.replaceAll(',', ';');
+
+console.log(yeniMetn);
+
+//Tapshiriq 2
+let qrup = ["Ayan", "Nigar", "Elshad"];
+qrup.unshift("Famil");
+qrup.pop();
+
+console.log(qrup);
